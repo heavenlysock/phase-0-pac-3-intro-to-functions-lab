357f0ba7db2c13c0d0ce64f45c43f3d69819bcf1
bobertsworld <bobertsworld@gmail.com>
2022-10-23 16:54:39 -0700

    commit tree 16bd6d631ed75bb200326e827516a40e547e24d9

---
+++ index.js
@@ -0,0 +1,15 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+
+function whisper(string) {
+    return string.toLowerCase();
+  }
+
+
+
+console.logshout(string);
+console.logshout(string.toUpperCase());
+
+console.logshout(string === string.toUpperCase());
